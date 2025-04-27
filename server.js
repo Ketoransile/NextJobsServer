ba23a580fabc2a63f83d3c9183c573df72bbe5ea
Ketoran Sileshi <abdisileshi123@gmail.com>
2025-04-27 16:02:29 +0300

    vercel.json added

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoute.js";
 import { authenticateUser } from "./middlewares/authenticateUser.js";
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://job-portal-omega-flame.vercel.app",
+];
 const app = express();
 
 //
@@ -12,33 +16,20 @@ app.use(express.json());
 app.use(clerkMiddleware());
 app.use(
   cors({
-    origin: "http://localhost:3000", // your frontend URL
-    credentials: true, // if you are using cookies or Authorization headers
+    origin: allowedOrigins,
+    credentials: true,
   })
 );
 
 // Routes
 app.use("/api/v1/users", requireAuth(), userRoutes);
-// app.use("/api/v1/auth", authRoutes);
-// app.use("/api/v1/jobs", requireAuth(), jobRoutes);
-// app.use("/api/v1/applications", requireAuth(), applicationRoutes);
-// app.use("/api/v1/company", requireAuth(), companyRoutes);
+
 console.log("process.env.PORT", process.env.PORT);
 console.log("process.env.MONGODB_URL", process.env.MONGODB_URL);
 app.get("/", (req, res) => {
   res.send("Hello world 🥳");
 });
-// app.get(
-//   "/first/api",
-//   requireAuth({ signInUrl: "/sign-in" }),
-//   async (req, res) => {
-//     const { userId } = getAuth(req);
 
-//     // Use Clerk's JavaScript Backend SDK to get the user's User object
-//     const user = await clerkClient.users.getUser(userId);
-//     res.json({ message: "SUccessfully fetched from backend", user });
-//   }
-// );
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`App is running in on port ${PORT}`);
