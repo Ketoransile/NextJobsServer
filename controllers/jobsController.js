b09c4f8420611cd8b3f006ff60de8a31410ba5b8
Ketoran Sileshi <abdisileshi123@gmail.com>
2025-05-03 00:37:47 +0300

    modified jobsController

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -4,13 +4,14 @@ import { Job } from "../models/Job.js";
 export const getAllJobs = async (req, res) => {
   try {
     const page = parseInt(req.query.page) - 1 || 0;
-    const limit = parseInt(req.query.limit) || 0;
+    const limit = parseInt(req.query.limit) || 4;
     const search = req.query.search || "";
     const location = req.query.location || "";
     const company = req.query.company || "";
     const category = req.query.category || "";
 
     await connectDB();
+
     const query = {};
     if (search) {
       query.$or = [
@@ -26,16 +27,27 @@ export const getAllJobs = async (req, res) => {
     let companyIds = null;
 
     if (company) {
-      const compnaies = await Company.find({
+      const companyFound = await Company.findOne({
         name: { $regex: company, $options: "i" },
-      }).select("_id");
-      companyIds = compnaies.map((c) => c._id);
-      query.companyId = { $in: companyIds };
+      });
+
+      if (!companyFound) {
+        return res
+          .status(404)
+          .json({ message: "No jobs found for this company" });
+      }
+
+      query.companyId = companyFound._id;
     }
+
     if (category) {
       query.category = { $regex: category, $options: "i" };
     }
     console.log("Query object is", query);
+    const totalJobs = await Job.countDocuments(query);
+    const totalPages = Math.ceil(totalJobs / limit);
+    console.log("Total jobs are ", totalJobs);
+    console.log("Total pages are ", totalPages);
     const jobs = await Job.find(query)
       .populate("companyId", "name iconUrl website description")
       .skip(page * limit)
@@ -45,9 +57,11 @@ export const getAllJobs = async (req, res) => {
     if (!jobs?.length) {
       return res.status(404).json({ message: "No jobs  found!!" });
     }
-    res
-      .status(200)
-      .json({ message: "Jobs are successfully fetched", data: jobs });
+    res.status(200).json({
+      message: "Jobs are successfully fetched",
+      data: jobs,
+      totalPages,
+    });
   } catch (error) {
     console.error("Error while fetching jobs", error);
     return res
