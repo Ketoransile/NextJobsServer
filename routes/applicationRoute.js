f95555e89d7b45d5d14985362bca4034fb5bbc41
Ketoran Sileshi <abdisileshi123@gmail.com>
2025-06-04 13:27:06 +0300

    multer storage updated

--- routes/applicationRoute.js
+++ routes/applicationRoute.js
@@ -7,15 +7,15 @@ import {
 } from "../controllers/applicationController.js";
 import multer from "multer";
 const router = express.Router();
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 router.post("/upload", upload.single("resume"), uploadResume);
